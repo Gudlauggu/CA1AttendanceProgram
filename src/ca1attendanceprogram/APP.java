6d4e7bf717a75f4204b187c176340df80c0ce25d
dani5636 <dani5636@easv365.dk>
2017-02-14 10:47:01 +0100

    s
    s

--- src/ca1attendanceprogram/APP.java
+++ src/ca1attendanceprogram/APP.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author Mecaa
  */
-public class APP extends Application {
+public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
