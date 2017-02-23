40f2bb987ddfc034f04e760ee0958ab9c21f94da
Gudlauggu <gudl0025@easv365.dk>
2017-02-23 17:05:10 +0100

    CB in teacher plus some table work

--- src/ca1attendanceprogram/GUI/Controller/TeacherOverviewController.java
+++ src/ca1attendanceprogram/GUI/Controller/TeacherOverviewController.java
@@ -8,9 +8,14 @@ package ca1attendanceprogram.GUI.Controller;
 import ca1attendanceprogram.BE.Student;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -31,6 +36,8 @@ public class TeacherOverviewController implements Initializable
     private TableColumn<Student, String> clmAbsence;
     @FXML
     private TableColumn<Student, String> clmAttending;
+    @FXML
+    private ComboBox<String> CBLesson;
 
     /**
      * Initializes the controller class.
@@ -40,6 +47,7 @@ public class TeacherOverviewController implements Initializable
       {
         makeAStudent();
         updateFields();
+        groupChoicer();
 
       }
 
@@ -60,6 +68,28 @@ public class TeacherOverviewController implements Initializable
           {
             tblAllLessons.getItems().add(new Student("stefaan" + i, "steven@gmail.com", 1 + i, "***", "Steven Nielssen"));
           }
+      }
+    
+    
+    public void addListener()
+      {
+        CBLesson.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>()
+        {
+            public void changed(ObservableValue ov, Number value, Number new_value)
+              {
+                CBLesson.getSelectionModel().select(new_value.intValue());
+                
+                
+                tblAllLessons.refresh();
+              }
+
+        });
+      }
+    
+    private void groupChoicer()//Sets the items in the choicebox
+      {
+        ObservableList<String> groups = FXCollections.observableArrayList("SCO", "ITO", "SDE");
+        CBLesson.setItems(groups);
       }
 
 }
