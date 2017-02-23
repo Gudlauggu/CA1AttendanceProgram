e1bda67522e4b72488ef90816cfbc5a00eeae891
dani5636 <dani5636@easv365.dk>
2017-02-23 18:28:22 +0100

    Sync once more
    ewqwe

---
+++ src/ca1attendanceprogram/GUI/Controller/AbsenceOverviewController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca1attendanceprogram.GUI.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mechaa
+ */
+public class AbsenceOverviewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void sendRequest(ActionEvent event) {
+    }
+
+    @FXML
+    private void logOff(ActionEvent event) {
+    }
+    
+}
