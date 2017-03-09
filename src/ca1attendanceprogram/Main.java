e9859e02cb062e9824d1c75c442be381f578ad1c
Gudlauggu <gudl0025@easv365.dk>
2017-03-09 14:39:19 +0100

    lots of changes

--- src/ca1attendanceprogram/Main.java
+++ src/ca1attendanceprogram/Main.java
@@ -1,7 +1,9 @@
 package ca1attendanceprogram;
 
+import ca1attendanceprogram.DAL.StudentHandler;
 import ca1attendanceprogram.GUI.Controller.LoginController;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -35,15 +37,12 @@ public class Main extends Application {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
+        
+        
     }
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+    
 }
 
 
